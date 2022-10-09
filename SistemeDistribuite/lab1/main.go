357b54ec81a6d8e3a00ea9ea2063a18975e10f9a
predaalex <preda.alex30@yahoo.com>
2022-10-09 13:03:48 +0300

    SD lab1

---
+++ SistemeDistribuite/lab1/main.go
@@ -0,0 +1,23 @@
+package main
+import ("fmt")
+
+// that's how to comment
+func main() {
+	// fmt.Println("Hello World!")
+
+	var stud1 string = "Jhon" //type is tring
+	var stud2 = "Jane" //type is inferred
+	x := 2 //type is inferred
+
+	fmt.Println(stud1)
+	fmt.Println(stud2)
+	fmt.Print(x)
+
+	var a string
+	var b int
+	var c bool
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+}
