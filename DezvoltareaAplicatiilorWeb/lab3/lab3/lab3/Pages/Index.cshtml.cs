f78cfec3781a84fc49a31618f7c0e8fd2f9b4d12
predaalex <preda.alex30@yahoo.com>
2022-10-20 15:59:42 +0300

    DAP LAB3 .net stuff

---
+++ DezvoltareaAplicatiilorWeb/lab3/lab3/lab3/Pages/Index.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace lab3.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+        public Stiri[] stiri = new Stiri[]
+        {
+            new Stiri("Titlu", "Lead", "Autor" , new DateTime(2022, 10, 20)),
+
+        };
+        public void OnGet()
+        {
+
+        }
+    }
+
+    public class Stiri
+    {
+        public String titlu;
+        public String lead;
+        public String autor;
+        public DateTime data;
+
+        public Stiri() { }
+        public Stiri(string titlu, string lead, string autor, DateTime data)
+        {
+            this.titlu = titlu;
+            this.lead = lead;
+            this.autor = autor;
+            this.data = data;
+        }
+    }
+
+}
